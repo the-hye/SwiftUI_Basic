f13b52f0dd7a3ef8f31d8a3afa85e1cb94beb6e2
jihye <ganzi410@gmail.com>
2024-04-17 11:36:41 +0900

    Initial Commit

---
+++ CarList/CarListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListApp.swift
+//  CarList
+//
+//  Created by JIHYE SEOK on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CarListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
