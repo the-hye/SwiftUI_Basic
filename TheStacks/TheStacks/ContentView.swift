d47a03ba189e71a7abe0f68390d0dfa00392f9e8
jihye <ganzi410@gmail.com>
2024-04-23 13:26:06 +0900

    VStack, HStack, ZStack

---
+++ TheStacks/TheStacks/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  TheStacks
+//
+//  Created by JIHYE SEOK on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("VStack Item 1")
+            Text("VStack Item 2")
+            Spacer()
+            Divider()
+                .background(.black)
+            Text("VStack Item 3")
+            
+            HStack {
+                Text("HStack Item 1")
+                Divider()
+                    .background(.black)
+                Spacer()
+                Text("HStack Item 2")
+                Divider()
+                    .background(.black)
+                Spacer()
+                Text("HStack Item 3")
+            }
+            .background(.yellow)
+            ZStack {
+                Text("ZStack Item 1")
+                    .padding()
+                    .background(.green)
+                    .opacity(0.8)
+                Text("ZStack Item 2")
+                    .padding()
+                    .background(.green)
+                    .offset(x: 80, y: -100)
+            }
+            .padding()
+        }
+        .background(.blue)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheStacks/TheStacks/TheStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStacksApp.swift
+//  TheStacks
+//
+//  Created by JIHYE SEOK on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TheStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
