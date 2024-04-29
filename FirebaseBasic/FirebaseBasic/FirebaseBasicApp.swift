dbab5f8c6811b46dcdfa965ca0f5d77d84eaba7b
jihye <ganzi410@gmail.com>
2024-04-29 13:39:07 +0900

    Initialize FirebaseBasic and update .gitignore

---
+++ FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBasicApp.swift
+//  FirebaseBasic
+//
+//  Created by JIHYE SEOK on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
